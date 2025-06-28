2df8d118f971c9eac3ebefd0e049f5ec0d1d33da
Bryce Campbell <tonyhawk2100@gmail.com>
2025-06-28 16:57:10 -0700

    add stuff needed for files.

--- src/entry.rs
+++ src/entry.rs
@@ -1,4 +1,5 @@
 use chrono::prelude::*;
+use std::{ ffi::OsStr, fs::File, path::Path };
 
 pub struct Entry {
     pub date: DateTime<Utc>,
@@ -18,5 +19,25 @@ impl Entry {
             date,
             content: content.to_owned()
         }
+    }
+
+    pub fn from_file(p: &Path) -> Option<Self> {
+        if let Some(date) = date_time_from_file(p) {
+
+        } else {
+            None
+        }
+    }
+}
+
+fn date_time_from_file(p: &Path) -> Option<DateTime<Utc>> {
+    if let Some(file) = p.file_stem().and_then(OsStr::to_str) {
+        if let Ok(date_time) = NaiveDateTime::parse_from_str(file, "%Y-&m-%d_%H-%M") {
+            Some(Utc.from_utc_datetime(&date_time))
+        } else {
+            None
+        }
+    } else {
+        None
     }
 }
