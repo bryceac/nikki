c14605f884d110d9bb9257b425d587eb3fbe2e08
Bryce Campbell <tonyhawk2100@gmail.com>
2025-06-29 11:13:36 -0700

    move common functionality out of helpers.

--- src/read.rs
+++ src/read.rs
@@ -1,1 +1,18 @@
 use clap::Parser;
+
+use crate::shared::convert_human_date_time_to_computer;
+
+#[derive(Parser)]
+pub struct Read {
+    #[clap(default_value = "~/.journal")]
+    pub journal: String,
+    pub entry: String
+}
+
+impl Read {
+    pub fn run(&self) {
+        if let Some(entry_date_time) = convert_human_date_time_to_computer(&self.entry) {
+
+        }
+    }
+}
--- src/shared.rs
+++ src/shared.rs
@@ -8,6 +8,18 @@ pub fn real_path(p: &str) -> String {
     }
 }
 
+pub fn convert_string_to_date_time(date_time: &str, format: &str) -> Option<DateTime<Local>> {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(date_time, format) {
+        if let Single(date_time) = Local.from_local_datetime(&date_time) {
+            Some(date_time)
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
+
 pub fn convert_computer_date_time_to_human(date_time: &str) -> Option<String> {
     if let Ok(date_time) = NaiveDateTime::parse_from_str(date_time, "%Y-%m-%d_%H-%M") {
         if let Single(date_time) = Local.from_local_datetime(&date_time) {
