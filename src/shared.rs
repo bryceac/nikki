ca03a8ce394f6c0a83b284142ad2017bdeabf594
Bryce Campbell <tonyhawk2100@gmail.com>
2025-06-29 09:47:19 -0700

    remove unreachable arm.

--- src/shared.rs
+++ src/shared.rs
@@ -3,7 +3,6 @@ use url_path::UrlPath;
 pub fn real_path(p: &str) -> String {
     match p {
         path if path.starts_with("~") => shellexpand::tilde(p).into_owned(),
-        path if path.contains("$") => shellexpand::env(path).expect("Could not get path").to_string(),
         _ => UrlPath::new(p).normalize()
     }
 }
