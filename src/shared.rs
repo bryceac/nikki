1eef97eaca99b8b7e429522c3cdfb814a82439dd
Bryce Campbell <tonyhawk2100@gmail.com>
2025-06-28 20:05:48 -0700

    make sure application supports variables.

--- src/shared.rs
+++ src/shared.rs
@@ -1,14 +1,9 @@
 use url_path::UrlPath;
 
 pub fn real_path(p: &str) -> String {
-    /* if p.starts_with("~") {
-        shellexpand::tilde(p).into_owned()
-    } else {
-        UrlPath::new(p).normalize()
-    } */
-
     match p {
         path if path.starts_with("~") => shellexpand::tilde(p).into_owned(),
+        path if path.contains("$") => shellexpand::env(path).expect("Could not get path").to_string(),
         _ => UrlPath::new(p).normalize()
     }
 }
