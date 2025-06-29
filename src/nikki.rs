396de58bdf4f4f08da62f2bfc90f5db6bd54eb88
Bryce Campbell <tonyhawk2100@gmail.com>
2025-06-28 17:37:42 -0700

    add in placeholder for subcommands.

--- src/nikki.rs
+++ src/nikki.rs
@@ -3,5 +3,6 @@ use clap::Parser;
 #[derive(Parser)]
 #[clap(version = "0.1.0", author = "Bryce Campbell <tonyhawk2100@gmail.com>")]
 pub struct Nikki {
-
+    #[clap(subcommand)]
+    pub subcommand: String
 }
