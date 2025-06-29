5e006e6679e4eeaa8c54b0f48da86f2f8e76dac0
Bryce Campbell <tonyhawk2100@gmail.com>
2025-06-29 15:10:43 -0700

    remove clap things from subcommand.

--- src/subcommand.rs
+++ src/subcommand.rs
@@ -3,10 +3,7 @@ use crate::{ List, Pen, Read };
 
 #[derive(Parser)]
 pub enum SubCommand {
-    #[clap(version = "0.1", author = "Bryce Campbell")]
     List(List),
-    #[clap(version = "0.1", author = "Bryce Campbell")]
     Pen(Pen),
-    #[clap(version = "0.1", author = "Bryce Campbell")]
     Read(Read)
 }
