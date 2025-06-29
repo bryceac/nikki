b3d2700388ccc1c5c928ad43e0ec2eff641ae197
Bryce Campbell <tonyhawk2100@gmail.com>
2025-06-28 17:41:20 -0700

    work on getting a base set up right.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod shared;
 mod entry;
 mod nikki;
+mod subcommand;
+
+use subcommand::SubCommand as SubCommand;
 
 fn main() {
     println!("Hello, world!");
--- src/nikki.rs
+++ src/nikki.rs
@@ -1,8 +1,10 @@
 use clap::Parser;
 
+use crate::SubCommand;
+
 #[derive(Parser)]
 #[clap(version = "0.1.0", author = "Bryce Campbell <tonyhawk2100@gmail.com>")]
 pub struct Nikki {
     #[clap(subcommand)]
-    pub subcommand: String
+    pub subcommand: SubCommand
 }
